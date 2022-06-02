af9950311e0bb0e7ec1682b6e1112bd95065e01c
nite <532702231@qq.com>
2022-06-02 10:16:44 +0800

    笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,22 +6,38 @@ module.exports = {
     nav: [
       { text: '首页', link: '/' },
       { text: '导航', link: '/guide/' },
+      { text: '笔记', link: '/node/' },
       { text: 'GitHub', link: 'https://github.com/ni5328109/web-road-map' }
     ],
-    sidebar: [
-      {
-        title: '前言',
-        sidebarDepth: 0,
-        collapsable: false,
-        children: ['/guide/']
-      },
-      {
-        title: '前端入门',
-        path:'/primary/',
-        collapsable: false,
-        children: ['/primary/html', '/primary/css', '/primary/js']
-      }
-    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: '前言',
+          sidebarDepth: 0,
+          collapsable: false,
+          children: ['/guide/']
+        },
+        {
+          title: '前端入门',
+          path: '/primary/',
+          collapsable: false,
+          children: ['/primary/html', '/primary/css', '/primary/js']
+        }
+      ],
+      '/node/': [
+        {
+          title: '笔记',
+          path: '/node/',
+          sidebarDepth: 0,
+          collapsable: false
+        },
+        {
+          title: 'JavaScript高级程序设计',
+          path: '/node/js-advanced'
+        }
+      ],
+      '/': ['' /* / */]
+    },
     sidebarDepth: 3,
     smoothScroll: true
   },
